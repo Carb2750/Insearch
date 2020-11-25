849978fe1f19edbd590596b34e75da29aaa42924
Carb2750 <carb2750@hotmail.com>
2020-11-25 16:31:13 -0600

    Remove Jwt Middleware

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-let passport = require("passport");
-let passportJWT = require("passport-jwt");
-
-let extractJWT = passportJWT.ExtractJwt;
-let strategyJWT = passportJWT.Strategy;
-
-passport.use(
-  new strategyJWT(
-    {
-      jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
-    },
-    (payload, next) => {
-      var user = payload;
-      return next(null, user);
-    }
-  )
-);
-
-const jwtAuthMiddleware = passport.authenticate("jwt", { session: false });
-
 const securityRoutes = require('./api/security');
 const WorksRoutes = require("./api/works");
 
 router.use('/security', securityRoutes);
-router.use("/works", jwtAuthMiddleware, WorksRoutes);
+router.use("/works", WorksRoutes);
 
 module.exports = router;
