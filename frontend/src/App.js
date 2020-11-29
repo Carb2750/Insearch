733855136f3f1b1211f74265b5712d30ef3bc3d3
Carb2750 <carb2750@hotmail.com>
2020-11-28 23:05:32 -0600

    Add Router and Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
+import Header from './components/commons/Header/index';
+import LandingPage from './components/public/Langinpage/LandingPage';
+import JobPage from './components/public/JobPage/JobPage';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/practicas" exact component={JobPage} />
+        <Route path="/login" exact component={null} />
+      </Switch>
     </div>
   );
 }
