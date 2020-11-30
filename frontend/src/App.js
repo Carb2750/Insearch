d7678eb50d45603e43da6da00100c280af62bacb
Carb2750 <carb2750@hotmail.com>
2020-11-30 16:51:57 -0600

    Add Jobs Posted Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Header from './components/commons/Header/index';
 import LandingPage from './components/public/Langinpage/LandingPage';
 import JobPage from './components/public/JobPage/JobPage';
 
+import JobPostedPage from './components/private/JobsPostedPage/JobPostedPage';
+
 const App = () => {
   return (
     <div>
@@ -13,6 +15,7 @@ const App = () => {
         <Route path="/" exact component={LandingPage} />
         <Route path="/practicas" exact component={JobPage} />
         <Route path="/login" exact component={null} />
+        <Route path="/postedjobs" exact component={JobPostedPage}/>
       </Switch>
     </div>
   );
