c28ecf7c32c7f6a13b2b2a09f5537a77b1e80c85
Carb2750 <carb2750@hotmail.com>
2020-12-14 09:12:33 -0600

    Add Create User

--- frontend/src/components/public/RegisterUserPage/PageOne.js
+++ frontend/src/components/public/RegisterUserPage/PageOne.js
@@ -4,17 +4,7 @@ import RegisterOne from '../../../components/commons/RegisterForm/RegisterUser/R
 import { privAxios } from '../../../utils/axios';
 
 const Register = () => {
-
-    /*const SignUp = (e) =>{
-        e.preventDefault();
-        e.stopPropagation();
-        privAxios.post('/api/security/signupuser', form)
-        .then((data)=>{
-            console.log(data);
-           //dispatch({ type: })
-        })
-    }*/
-
+    
     return (
         <StyledDiv>
             <StyledDivImage />
