fa1dc8a75c2c9c7317d1e614320be7772ccd58da
Carb2750 <carb2750@hotmail.com>
2020-12-15 12:16:27 -0600

    Add Footer

---
+++ frontend/src/components/commons/Footer/index.js
@@ -0,0 +1,9 @@
+import { StyledDiv } from './style';
+
+const footer = () => (
+    <StyledDiv>
+        <p>Todos los derechos reservados 2020&#169;.</p>
+    </StyledDiv>
+);
+
+export default footer;
---
+++ frontend/src/components/commons/Footer/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 3.5rem;
+    padding: 0 3rem 0 3rem;
+    background-color: #1D8027;
+    z-index: 100;
+
+    & p {
+        color: #ffffff;
+        font-size: 1.2rem;
+    }
+`
