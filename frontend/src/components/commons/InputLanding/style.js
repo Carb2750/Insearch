78ee121e08a47792bb02d8a0363d187b44ab08ea
Omar SF <60301579+Ofrings@users.noreply.github.com>
2020-12-15 00:46:09 -0600

    Add Input

---
+++ frontend/src/components/commons/InputLanding/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  width: 100%;
+`;
+
+export const StyledP = styled.p`
+  font-size: 3.4rem;
+  margin-bottom: 1rem;
+  display: inline-block;
+  width: 100%;
+`;
+
+export const StyledInput = styled.input`
+  border: none;
+  border-radius: 7px;
+  box-shadow: 0px 1px 1px #444444;
+
+  margin-bottom: 1.2rem;
+
+  width: ${(props) => props.size};
+  height: 2.4rem;
+`;
